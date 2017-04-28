0c50d3acb5fb970e0c15eb38e3826f46319409f8
Dmitry Tochansky <tochansky@tochlab.net>
2017-04-28 16:29:57 +0300

    Upgrade makefiles

--- zlib.mk
+++ zlib.mk
@@ -4,5 +4,5 @@ ZLIBVERSION=1.2.11
 default:
 	mkdir -p $(BUILDDIR)
 	mkdir -p $(OUTPUTDIR)
-	tar -xvf sources/zlib-$(ZLIBVERSION).tar.gz -C $(BUILDDIR)
+	tar -xf sources/zlib-$(ZLIBVERSION).tar.gz -C $(BUILDDIR)
 	cd $(BUILDDIR)/zlib-$(ZLIBVERSION); ./configure --prefix=/usr; make; make DESTDIR=$(OUTPUTDIR) install
