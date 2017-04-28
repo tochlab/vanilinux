38d49c3b6c3df990220a9e9de7491ebd15144286
Dmitry Tochansky <tochansky@tochlab.net>
2017-04-28 17:06:28 +0300

    makefile update.

--- zlib.mk
+++ zlib.mk
@@ -4,5 +4,8 @@ ZLIBVERSION=1.2.11
 default:
 	mkdir -p $(BUILDDIR)
 	mkdir -p $(OUTPUTDIR)
-	tar -xf sources/zlib-$(ZLIBVERSION).tar.gz -C $(BUILDDIR)
+	mkdir -p $(PKGDIR)
+	$(call extract_archive,zlib-$(ZLIBVERSION).tar.gz)
 	cd $(BUILDDIR)/zlib-$(ZLIBVERSION); ./configure --prefix=/usr; make; make DESTDIR=$(OUTPUTDIR) install
+	$(call create_pkg,zlib-$(ZLIBVERSION))
+	$(call clean_workdirs)
