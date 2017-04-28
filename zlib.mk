b9a84d82964ab11f9e4adb07bbeb152178ddba3a
Dmitry Tochansky <tochansky@tochlab.net>
2017-04-28 17:11:54 +0300

    .ONESHELL!

--- zlib.mk
+++ zlib.mk
@@ -6,6 +6,9 @@ default:
 	mkdir -p $(OUTPUTDIR)
 	mkdir -p $(PKGDIR)
 	$(call extract_archive,zlib-$(ZLIBVERSION).tar.gz)
-	cd $(BUILDDIR)/zlib-$(ZLIBVERSION); ./configure --prefix=/usr; make; make DESTDIR=$(OUTPUTDIR) install
+	cd $(BUILDDIR)/zlib-$(ZLIBVERSION)
+	./configure --prefix=/usr
+	make
+	make DESTDIR=$(OUTPUTDIR) install
 	$(call create_pkg,zlib-$(ZLIBVERSION))
 	$(call clean_workdirs)
